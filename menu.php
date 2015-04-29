9ca728156bf2cc8708bd36a762dea61ed248cc91
jje001 <jje001@uark.edu>
2015-04-28 23:02:33 -0500

    Changed the menu

--- menu.php
+++ menu.php
@@ -4,15 +4,14 @@
 <link rel="stylesheet" href="menu.css" type="text/css"/>
 <script type="text/javascript" src="http://code.jquery.com/jquery.min.js"></script>
 <script type="text/javascript" src="menu.js"></script>
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=yes"/>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=3, user-scalable=no"/>
+
 </head>
 <body>
         <div class="rmm">
             <ul>
                 <li><a href="main.php">HOME</a></li>
                 <li><a href="settings.php">SETTINGS</a></li>
-                <li><a href="account.php">LOGIN</a></li>
-                <li><a href="register.php">REGISTER</a></li>
                 <li><a href="logout.php">LOGOUT</a></li>
                 <li><a href="upload.php">UPLOAD FILE</a></li>
                 <li><a href="messages.php"> VIEW MESSAGES</a></li>
---
+++ menu_guest.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="menu.css" type="text/css"/>
+<script type="text/javascript" src="http://code.jquery.com/jquery.min.js"></script>
+<script type="text/javascript" src="menu.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
+</head>
+<body>
+        <div class="rmm">
+            <ul>
+                <li><a href="main.php">HOME</a></li>
+                <li><a href="account.php">LOGIN</a></li>
+                <li><a href="register.php">REGISTER</a></li>
+            </ul>
+        </div>
+</body>
+</html>
